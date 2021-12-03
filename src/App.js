8e04c4976d979a93c262046269f5da67f3369354
BrendaQu <brenda.quach@colorado.edu>
2021-12-03 09:30:30 -0700

    user navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+//import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import NavbarUser from './componenets/NavbarUser';
 
 function App() {
   return (
     <div>
-      
+        <NavbarUser />
     </div>
   );
 }
