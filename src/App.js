f1b014e6187b1c6a77787d2e9095db1ac6cfdc42
MBaidoum <meanturtles@hotmail.com>
2021-12-05 11:20:39 -0800

    Me am dumb dumb

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import NavbarDefault from './componenets/NavbarDefault';
 import EmployeeApprovalPage from './pages/EmployeeApprovalPage';
@@ -8,6 +7,12 @@ import EmployeeMenuPage from './pages/EmployeeMenuPage';
 import LandingPage from './pages/LandingPage';
 import EmployeeSearchPage from './pages/EmployeeSearchPage';
 import EmployeeReportPage from './pages/EmployeeReportPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import UserBudgetPage from './pages/UserBudgetPage';
+import UserExpensePage from './pages/UserExpensePage';
+import UserMenuPage from './pages/UserMenuPage';
+import UserReportPage from './pages/UserReportPage';
 
 function App() {
   return (
@@ -20,34 +25,16 @@ function App() {
           <Route path='/EmpMenu' element={<EmployeeMenuPage />} />
           <Route path='/EmpSearch' element={<EmployeeSearchPage />} />
           <Route path='/EmpReport' element={<EmployeeReportPage />} />
+          <Route path='/userbudget' element={<UserBudgetPage />} />
+          <Route path='/userexpense' element={<UserExpensePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/usermenu' element={<UserMenuPage />} />
+          <Route path='/userreport' element={<UserReportPage />} />
           
         </Routes>
       </div>
     </Router>
-=======
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import UserBudgetPage from './pages/UserBudgetPage';
-import UserExpensePage from './pages/UserExpensePage';
-import UserMenuPage from './pages/UserMenuPage';
-import UserReportPage from './pages/UserReportPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-function App() {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/userbudget' element={<UserBudgetPage />} />
-          <Route path='/userexpense' element={<UserExpensePage />} />
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/usermenu' element={<UserMenuPage />} />
-          <Route path='/userreport' element={<UserReportPage />} />
-        </Routes>
-      </Router>
-    </div>
->>>>>>> origin/brenda
   );
 }
 
