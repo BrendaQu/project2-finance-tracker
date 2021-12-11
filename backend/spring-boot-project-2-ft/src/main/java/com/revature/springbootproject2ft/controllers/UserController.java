2854a424e3c30534e5820f6b9f4c2bf10c51957a
BrendaQu <brenda.quach@colorado.edu>
2021-12-11 16:51:29 -0700

    updated new backend

--- backend/spring-boot-project-2-ft/src/main/java/com/revature/springbootproject2ft/controllers/UserController.java
+++ backend/spring-boot-project-2-ft/src/main/java/com/revature/springbootproject2ft/controllers/UserController.java
@@ -1,4 +1,44 @@
 package com.revature.springbootproject2ft.controllers;
 
+import com.revature.springbootproject2ft.entities.User;
+import com.revature.springbootproject2ft.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/users")
+    public User saveUser(@RequestBody User user) {
+        return service.addUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable("id") long id){
+        return service.getUserById(id);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable("id") long id){
+        service.deleteUser(id);
+        return "record deleted successfully";
+    }
+
+    @PutMapping("/users/{id}")
+    public String updateUser(@PathVariable("id") long id, @RequestBody User user){
+        service.updateUser(id, user);
+        return "record updated successfully";
+    }
+
+
+
 }
