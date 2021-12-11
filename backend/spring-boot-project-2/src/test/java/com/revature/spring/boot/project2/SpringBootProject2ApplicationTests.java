0829eb09f3fd5aadb0a1f1d5c4680963729afa6b
BrendaQu <brenda.quach@colorado.edu>
2021-12-11 14:30:16 -0700

    test

---
+++ backend/spring-boot-project-2/src/test/java/com/revature/spring/boot/project2/SpringBootProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.spring.boot.project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
