28737b5849fc6050ef14b461fd657399745634a3
BrendaQu <brenda.quach@colorado.edu>
2021-12-14 15:21:50 -0700

    added previous month to user report

--- frontend/src/componenets/UserBudgetForm/index.js
+++ frontend/src/componenets/UserBudgetForm/index.js
@@ -15,14 +15,15 @@ const UserBudgetForm = () => {
     })
 
     function onSubmitHandler(e) {
+        e.preventDefault()
         axios.get('http://localhost:9007/budgets/' + sessionStorage.getItem("userId"), budget)
         .then(response => {
             if(!response.data || response.data.length == 0){
                 axios.post('http://localhost:9007/budgets/' + sessionStorage.getItem("userId"), budget)
                 .then(response => {
-                    window.location.pathname = ('/userbudget')
                     setBudget(response.data)
                     console.log(response.data)
+                    window.location.pathname = ('/userbudget')
                 },
                     error => {
                         console.log(error)
@@ -31,9 +32,9 @@ const UserBudgetForm = () => {
             } else {
                 axios.put('http://localhost:9007/budgets/' + sessionStorage.getItem("userId"), budget)
                 .then(response => {
-                    window.location.pathname = ('/userbudget')
                     setBudget(response.data)
                     console.log(response.data)
+                    window.location.pathname = ('/userbudget')
                 },
                     error => {
                         console.log(error)
@@ -49,6 +50,14 @@ const UserBudgetForm = () => {
             [event.target.name]: event.target.value
         });
     }
+
+    useEffect(() => {
+        axios.get("http://localhost:9007/budgets/" + sessionStorage.getItem("userId"), budget)
+        .then((response) => {
+            setBudget(response.data);
+        })
+        .catch((error) => console.error(error));
+    }, [])
 
     return (
         <div className="container">
