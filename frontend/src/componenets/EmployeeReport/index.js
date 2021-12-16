fd260d706eaf449938f501171a0d53b82e47edb2
MBaidoum <meanturtles@hotmail.com>
2021-12-15 21:51:08 -0800

    Employee report 'fixed' within reason

--- frontend/src/componenets/EmployeeReport/index.js
+++ frontend/src/componenets/EmployeeReport/index.js
@@ -1,3 +1,179 @@
+// //Milton
+// import React, { useState, useEffect } from "react";
+// import { Bar, Doughnut } from "react-chartjs-2";
+// import { Chart, registerables } from 'chart.js'
+// import axios from "axios";
+
+
+// Chart.register(...registerables);
+// Chart.overrides.doughnut = {
+//     plugins: {
+//         title: {
+//             display: false,
+//             text: "Please Work"
+//         },
+//         legend: {
+//             display: false,
+//             position: "right"
+//         }
+//     }
+
+// }
+
+// const EmployeeReport = () => {
+
+//     const catSums = {
+//         Rent: 0,
+//         Utilities: 0,
+//         Groceries: 0,
+//         Subscriptions: 0,
+//         Entertainment: 0,
+//         Dining: 0,
+//         Shopping: 0,
+//         Misc: 0
+//     }
+
+//     const [category, setCategory] = useState([{
+//         rent: "",
+//         utilities: "",
+//         groceries: "",
+//         subscriptions: "",
+//         entertainment: "",
+//         dining: "",
+//         shopping: "",
+//         misc: ""
+//     }])
+
+//     const [catTotals, setCatTotals] = useState([])
+
+//     const [count, setCount] = useState([])
+
+//     const chartData = {
+//         labels: ['Rent', 'Utilities', 'Groceries', 'Subscriptions', 'Entertainment', 'Dining', 'Shopping', 'Misc'],
+//         datasets: [
+//             {
+//                 label: 'Dataset 1',
+//                 data: Object.values(category),
+//                 backgroundColor: ['#7ABA30', '#ffce39', '#f3994a', '#266482', '#6832AD', '#F3524A', '#2E8B94', '#AA26A4']
+//             }
+//         ]
+//     };
+
+//     const options = {
+//         plugins: {
+//             title: {
+//                 display: false,
+//                 text: "Please Work"
+//             },
+//             legend: {
+//                 display: false,
+//                 position: "bottom"
+//             },
+//             responsive: true
+//         },
+
+//     }
+
+//     const onClickBar = () => {
+//         var x = document.getElementsByClassName("bar-graph")[0];
+//         var y = document.getElementsByClassName("doughnut-graph")[0];
+//         console.log(x)
+//         if (x.style.display === 'none') {
+//             y.style.display = 'none';
+//             x.style.display = 'block';
+//         } else {
+//             x.style.display = 'none';
+//             y.style.display = 'block';
+//         }
+//     }
+
+//     const onClickDoughnut = () => {
+//         var x = document.getElementsByClassName("doughnut-graph")[0];
+//         var y = document.getElementsByClassName("bar-graph")[0];
+//         console.log(x)
+//         if (x.style.display === 'none') {
+//             y.style.display = 'none';
+//             x.style.display = 'block';
+//         } else {
+//             x.style.display = 'none';
+//             y.style.display = 'block';
+//         }
+//     }
+
+//     const onLoadHandler = (obj) => {
+//         //let obj = catTotals
+//         console.log(catTotals)
+//         console.log(count + "d")
+//         count = 1
+//         for (let i = 0; i < obj.length; i++) {
+//             switch (obj[i].category) {
+//                 case "Rent":
+//                     catSums.Rent += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 case "Utilities":
+//                     catSums.Utilities += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 case "Groceries":
+//                     catSums.Groceries += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 case "Subscription":
+//                     catSums.Subscriptions += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 case "Entertainment":
+//                     catSums.Entertainment += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 case "Dining Out":
+//                     catSums.Dining += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 case "Shopping":
+//                     catSums.Shopping += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 case "Misc.":
+//                     catSums.Misc += Math.round(obj[i].amount / count * 100) / 100
+//                     break;
+//                 default:
+//                     break;
+//             }
+//         }
+//         setCategory(catSums);
+//     }
+
+//     useEffect(() => {
+//         var x = document.getElementsByClassName("doughnut-graph")[0];
+//         x.style.display = 'none';
+//         axios
+//             .get("http://localhost:9007/users/type/regular")
+//             .then((response) => {
+//                 console.log(response.data)
+//                 setCount(response.data)
+//             })
+//             .catch((error) => console.error(error))
+
+//         axios
+//             .get("http://localhost:9007/expenses")
+//             .then((response) => {
+//                 setCatTotals(response.data)
+//                 onLoadHandler()
+//             })
+//             .catch((error) => console.error(error));
+//     }, [])
+
+//     return (
+//         <div className="graphs container">
+//             <div className="bar-graph" onLoad={onLoadHandler}>
+//                 <button type="button" className="btn btn-primary m-3" onClick={onClickDoughnut}>Doughnut</button>
+//                 <Bar data={chartData} options={options} />
+//             </div>
+//             <div className="doughnut-graph">
+//                 <button type="button" className="btn btn-primary m-3" onClick={onClickBar}>Bar</button>
+//                 <Doughnut data={chartData} options={options} />
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default EmployeeReport
+
 //Milton
 import React, { useState, useEffect } from "react";
 import { Bar, Doughnut } from "react-chartjs-2";
@@ -22,7 +198,7 @@ Chart.overrides.doughnut = {
 
 const EmployeeReport = () => {
 
-    var count = 0
+    var count = 1
 
     const catSums = {
         Rent: 0,
@@ -102,13 +278,9 @@ const EmployeeReport = () => {
         var x = document.getElementsByClassName("doughnut-graph")[0];
         x.style.display = 'none';
         axios
-            .get("http://localhost:9007/users")
+            .get("http://localhost:9007/users/type/regular")
             .then((response) => {
-                for (let i = 0; i < response.data.length; i++) {
-                    if (response.data[i].type === "regular") {
-                        count++
-                    }
-                }
+                count=response.data
             })
             .catch((error) => console.error(error))
         axios
